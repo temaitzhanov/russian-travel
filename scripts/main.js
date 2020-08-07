a5112e2d891f410cf6c0ce06ea17467885ae5821
Temirlan Aitzhanov <tem.aitzhanov@yandex.kz>
2020-08-08 00:40:18 +0600

    Popup +2 event

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,15 @@
 const popup = document.querySelector('.popup');
 const popupOpenButton = document.querySelector('.header__popup-open');
 const popupCloseButton = popup.querySelector('.popup__close');
+console.log({
+  popup,
+  popupCloseButton,
+  popupOpenButton
+})
 const popupToggle = function(){
-  popup.classList.toggle('popup_open');
+  popup.classList.toggle('popup_opened');
 }
-popupToggle();
+popupOpenButton.addEventListener('click', popupToggle);
+popupCloseButton.addEventListener('click', popupToggle);
 
 
