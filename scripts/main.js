2d71b0ec7220d8986d38b2622f89f5b0bbfbaf96
Temirlan Aitzhanov <tem.aitzhanov@yandex.kz>
2020-08-08 15:00:32 +0600

    Закончена реализация popup

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,19 @@
 const popup = document.querySelector('.popup');
 const popupOpenButton = document.querySelector('.header__popup-open');
 const popupCloseButton = popup.querySelector('.popup__close');
-console.log({
-  popup,
-  popupCloseButton,
-  popupOpenButton
-})
-const popupToggle = function(){
+const root = document.querySelector('.root');
+console.log(root);
+const popupToggle = function(event){
+  event.preventDefault();
   popup.classList.toggle('popup_opened');
+}
+const closePopup = function(){
+  if(event.target !== event.currentTarget){
+    return;
+  }
+  popupToggle(event);
 }
 popupOpenButton.addEventListener('click', popupToggle);
 popupCloseButton.addEventListener('click', popupToggle);
-
+popup.addEventListener('click', closePopup)
 
